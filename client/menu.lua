8aec974c2fd0207cf83a3c0b0d2e80c302e3a7bb
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-02-02 23:05:51 +1100

    refactor(menu): move entry display to a function

--- client/menu.lua
+++ client/menu.lua
@@ -2,91 +2,91 @@ local tabs = {
 	modes = textModes,
 }
 
-local controlWidth = 0.3
-
+local width = 0.3
 local vein = exports.vein
-local menu
+local menu = false
+local page, pages, entries, entriesPerPage
+local firstEntry, lastEntry
+local heading = 'Brown Thunder Menu'
+
+tabs.modes.display = function(entry, i)
+	vein:separator(width)
+
+	vein:label(entry.name .. ' - ' .. entry.getTargets)
+
+	vein:textArea(entry.description, width)
+	
+	vein:beginRow()
+		if vein:button('Launch') then
+			menu = not menu
+			endMode()
+			TriggerServerEvent('brownThunder:nextRound', i, 1)
+		end
+
+		if vein:button('Details') then
+		end
+	vein:endRow()
+end
 
 RegisterCommand('btMenu', function(source, args, rawCommand)
-	if menu then
-		menu = false
-		return
-	end
-	menu = rawCommand
-	local tab = 'modes'
-	local page = 1
-	local entries = tabs[tab]
-	local entriesPerPage = 4
-	local pages = math.ceil(#entries / entriesPerPage)
-	local firstEntry
-	local lastEntry
+	menu = not menu
 
-	local windowX
-	local windowY
+	local windowX, windowY
 
-	while menu == rawCommand do
+	while menu do
 		Wait(0)
-
-		firstEntry = page * entriesPerPage - entriesPerPage + 1
-		lastEntry = math.min(firstEntry + entriesPerPage - 1, #entries)
 
 		vein:beginWindow(windowX, windowY)
 
-		vein:heading('Brown Thunder Controls')
+		vein:heading(heading)
 
 		vein:beginRow()
 			if vein:button('Modes') then
-				tab = 'modes'
+				entries = tabs.modes
+				entriesPerPage = 4
+				pages = math.ceil(#entries / entriesPerPage)
+				page = 1
+				heading = 'Brown Thunder Menu'
 			end
         vein:endRow()
 
+		if entries then
+			firstEntry = page * entriesPerPage - entriesPerPage + 1
+			lastEntry = math.min(firstEntry + entriesPerPage - 1, #entries)
 
-		for i = firstEntry, lastEntry do
-			vein:separator(controlWidth)
+			for i = firstEntry, lastEntry do
+				entries.display(entries[i], i)
+			end
 
-			vein:textArea(entries[i].name .. ' - ' .. entries[i].description, controlWidth)
+			vein:separator(width)
 
 			vein:beginRow()
-                if vein:button('Launch') then
-                    menu = false
-					endMode()
-                    TriggerServerEvent('brownThunder:nextRound', i, 1)
-                end
+				if vein:button('Close') then
+					menu = false
+				end
 
-                if vein:button('Details') then
-                end
-        	vein:endRow()
+				if vein:button('&lt;') then
+					if page > 1 then
+						page -= 1
+					end
+				end
+
+				if vein:button(page .. ' of ' .. pages) then
+				end
+
+				if vein:button('>') then
+					if page < pages then
+						page += 1
+					end
+				end
+
+				if activeMode then
+					if vein:button('End Mode') then
+						endMode()
+					end
+				end
+			vein:endRow()
 		end
-
-		vein:separator(controlWidth)
-
-		vein:beginRow()
-			if vein:button('Close') then
-				menu = false
-			end
-
-			if vein:button('&lt;') then
-				if page > 1 then
-					page -= 1
-				end
-			end
-
-			if vein:button(tostring(page)) then
-			end
-
-			if vein:button('>') then
-				if page < pages then
-					page += 1
-				end
-			end
-
-			if activeMode then
-				if vein:button('End Mode') then
-					endMode()
-				end
-			end
-		vein:endRow()
-		
 
 		windowX, windowY = vein:endWindow()
 	end
